8f50007ad38e58708eac415b30a945cf43ae1f39
tzhangchi <terry.zhangchi@outlook.com>
2023-02-18 11:09:48 +0800

    support scan and publish, refactor code

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,12 +1,12 @@
 require('dotenv').config();
 import { expect, Page } from '@playwright/test';
+import fs from 'fs';
+
+import { removeRootName, scanDirs } from './libs/file-logic';
 import { loadPage } from './libs/load-page';
 import { newPage } from './libs/page-logic';
 import { test } from './libs/playwright';
 import { createWorkspace } from './libs/workspace-logic';
-import fs from 'fs';
-import path from 'path';
-import { scanDirs } from 'libs/file-logic';
 loadPage();
 const token = process.env.token;
 let SYNC_CLOUD_TIME = 5000;
@@ -14,7 +14,7 @@ test.describe('create cloud workspace by different ways of importing ', () => {
   test('import by markdowns', async ({ page }) => {
     console.log(page.url());
 
-    await passAuth(_page);
+    await passAuth(page);
     console.log('passAuth success');
 
     await createWorkspace(
@@ -25,7 +25,7 @@ test.describe('create cloud workspace by different ways of importing ', () => {
     await batchCreatePagesByNotes(page);
     console.log('batchCreatePagesByNotes success');
 
-    await enableCloud(page);
+    await enableCloudAndPublic(page);
     console.log('enableCloud success');
   });
 });
@@ -59,20 +59,29 @@ const createPage = async (_page: Page, title: string, content: string) => {
   await _page.getByRole('paragraph').fill(content);
 };
 
-const enableCloud = async (_page: Page) => {
+const enableCloudAndPublic = async (_page: Page) => {
   await _page.getByRole('link', { name: 'Settings' }).click();
   await _page.getByText('Collaboration').click();
   await _page.getByRole('button', { name: 'Enable AFFiNE Cloud' }).click();
   await _page.getByRole('button', { name: 'Enable' }).click();
   await _page.waitForTimeout(SYNC_CLOUD_TIME);
+
+  await _page.getByRole('link', { name: 'Settings' }).click();
+  await _page.getByText('Publish').click();
+  await _page.getByRole('button', { name: 'Publish to web' }).click();
+
+  console.log(
+    'enableCloudAndPublic success ' + _page.getByRole('textbox').textContent()
+  );
 };
 const batchCreatePagesByNotes = async (_page: Page) => {
   const filesList = scanDirs('./notes').filesList;
+  const pageTiles: string[] = removeRootName(filesList, './notes');
   SYNC_CLOUD_TIME = filesList.length * 5000 + 5000;
   for (let i = 0; i < filesList.length; i++) {
     const file = filesList[i];
-    const pageContent = fs.readFileSync(path.join('./notes', file), 'utf-8');
-    const pageTitle = file;
+    const pageContent = fs.readFileSync(file, 'utf-8');
+    const pageTitle = pageTiles[i];
     await createPage(_page, pageTitle, pageContent);
   }
 };
